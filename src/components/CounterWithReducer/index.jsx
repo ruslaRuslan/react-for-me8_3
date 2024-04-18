d6bb88d917354ce8e1d57e4b9435c7893c66b504
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-04-19 01:57:27 +0400

    switch hazir

--- src/components/CounterWithReducer/index.jsx
+++ src/components/CounterWithReducer/index.jsx
@@ -1,8 +1,18 @@
 import React, { useReducer } from "react";
 
 const counterReducer = (count, action) => {
-
-    
+  switch (action.type) {
+    case "inc":
+      return count + 1;
+    case "dec":
+      return count + 1;
+    case "reset":
+      return count - count;
+    case "special":
+      const newEd = prompt("ededi daxil et");
+    default:
+      throw new Error("YALNIS!");
+  }
 };
 
 const CounterWithReducer = () => {
@@ -10,15 +20,11 @@ const CounterWithReducer = () => {
   return (
     <>
       <h1>{count}</h1>
-      <button onClick={()=>dispatch({type: 'inc'})}>+</button>
-      <button onClick={()=>dispatch({type: 'dec'})}>-</button>
-      <button onClick={()=>dispatch({type: 'reset'})}>0</button>
-      <button onClick={()=>dispatch({type: 'special'})}>special</button>
-      <button onClick={()=> console.log(count)}>console</button>
-
-
-
-
+      <button onClick={() => dispatch({ type: "inc" })}>+</button>
+      <button onClick={() => dispatch({ type: "dec" })}>-</button>
+      <button onClick={() => dispatch({ type: "reset" })}>0</button>
+      <button onClick={() => dispatch({ type: "special" })}>special</button>
+      <button onClick={() => console.log(count)}>console</button>
     </>
   );
 };
