d97e3e6f64164e162732808001b35acc75c3b760
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-04-19 01:52:40 +0400

    button-lar hazir

--- src/components/CounterWithReducer/index.jsx
+++ src/components/CounterWithReducer/index.jsx
@@ -1,12 +1,24 @@
 import React, { useReducer } from "react";
 
-const counterReducer = (count, action) => {};
+const counterReducer = (count, action) => {
+
+    
+};
 
 const CounterWithReducer = () => {
   const [count, dispatch] = useReducer(counterReducer, 0);
   return (
     <>
       <h1>{count}</h1>
+      <button onClick={()=>dispatch({type: 'inc'})}>+</button>
+      <button onClick={()=>dispatch({type: 'dec'})}>-</button>
+      <button onClick={()=>dispatch({type: 'reset'})}>0</button>
+      <button onClick={()=>dispatch({type: 'special'})}>special</button>
+      <button onClick={()=> console.log(count)}>console</button>
+
+
+
+
     </>
   );
 };
