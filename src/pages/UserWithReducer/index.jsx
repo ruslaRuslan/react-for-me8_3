721351b6bf08cf7f14552d45e70411d93dfa2cb7
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-04-18 13:25:53 +0400

    users-with-reduce

--- src/pages/UserWithReducer/index.jsx
+++ src/pages/UserWithReducer/index.jsx
@@ -1,12 +1,40 @@
 import { useReducer } from "react";
 
-const reduser = (state, action) => {};
+const reduser = (state, action) => {
+  if (action === "add_user") {
+    return state.concat(["ali", "idris"]);
+  } 
+  else if (action === "remove_user") {
+    return [];
+  }
+ 
+};
 
 const UserWithReducer = ({}) => {
-  const [state, dispatch] = useReducer(reduser, {});
-  return <>
-  
-  </>;
+  const [state, dispatch] = useReducer(reduser, ["orxan"]);
+  return (
+    <>
+      <button
+        onClick={() => {
+          dispatch("add_user");
+        }}
+      >
+        add user
+      </button>
+
+      <button
+        onClick={() => {
+          dispatch("remove_user");
+        }}
+      >
+        remove user
+      </button>
+
+      {state.map((ad) => (
+        <p key={ad}>{ad}</p>
+      ))}
+    </>
+  );
 };
 
 export default UserWithReducer;
