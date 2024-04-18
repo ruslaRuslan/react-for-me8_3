a35eac80d16007cbed3d66516b3e2ebd72dd3542
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-04-19 01:33:35 +0400

    reducer.js

--- src/pages/UserWithReducer/index.jsx
+++ src/pages/UserWithReducer/index.jsx
@@ -3,30 +3,7 @@ import { useEffect, useReducer } from "react";
 import {v4 as genId} from "uuid"
 const url = "https://jsonplaceholder.typicode.com/users/";
 
-const usersReducer = (state, action) => {
-  switch (action.type) {
-    case "get_users":
-      return action.payload;
-    case "remove_user":
-      return state.filter((user) => user.id !== action.payload);
-    case "edit_user":
-      return state.map((user) => {
-        if (user.id === action.payload) {
-          const newUsername = prompt("enter new user name:", user.username);
-          return { ...user, username: newUsername };
-        }
-        return user;
-      });
-      case "add_user":
-        const username = prompt('enter new username:')
-        const id = genId()
-        return state.concat({id, username})
-        case 'remove_first' :
-          return state.slice(1)
-    default:
-      throw new Error("YALNIS!!!!!!!");
-  }
-};
+
 
 const UsersWithReducer = ({}) => {
   const [state, dispatch] = useReducer(usersReducer, []);
---
+++ src/pages/UserWithReducer/reducer.js
@@ -0,0 +1,24 @@
+const usersReducer = (state, action) => {
+    switch (action.type) {
+      case "get_users":
+        return action.payload;
+      case "remove_user":
+        return state.filter((user) => user.id !== action.payload);
+      case "edit_user":
+        return state.map((user) => {
+          if (user.id === action.payload) {
+            const newUsername = prompt("enter new user name:", user.username);
+            return { ...user, username: newUsername };
+          }
+          return user;
+        });
+        case "add_user":
+          const username = prompt('enter new username:')
+          const id = genId()
+          return state.concat({id, username})
+          case 'remove_first' :
+            return state.slice(1)
+      default:
+        throw new Error("YALNIS!!!!!!!");
+    }
+  };
